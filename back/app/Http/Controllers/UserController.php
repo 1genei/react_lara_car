c066579ffcbcaf6a00ba60bfda1db9584b09084a
Jean Philippe <bongojeanphilippe@gmail.com>
2022-05-31 14:12:07 +0200

    register validator

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //
     public function register(Request $request) {
+    
+    
+        $validator = Validator::make($request->all(),[
+            "name" => "required|string",
+            "email" => "required|email|unique:users",
+            "password" => "required|string|min:4"
+        ]);
+        
+        if($validator->fails()){
+            return Response()->json([
+                "error" => $validator->errors(),
+                "status" => 400
+
+            ]);
+        }
+        
         $user = User::create([
             "name" => $request->input("name"),
             "email" => $request->input("email"),
@@ -19,6 +36,6 @@ class UserController extends Controller
         return Response()->json([
             "message" => "Profil créé",
             "status" => 200
-        ]);
+        ], 200);
     }
 }
