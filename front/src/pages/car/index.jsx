81d5e9532158cee4db8be0aca063518044abf27e
Jean Philippe <bongojeanphilippe@gmail.com>
2022-06-02 16:25:16 +0200

    petit fix bouton delete

--- front/src/pages/car/index.jsx
+++ front/src/pages/car/index.jsx
@@ -11,73 +11,65 @@ import { useSelector } from 'react-redux';
 function IndexCar() {
     const [cars, setCars] = useState([]);
     
-    const navigate = useNavigate();
     const auth  = useSelector( (state) =>state.auth);
 
     function deleteHandle(e){
       e.preventDefault();
-      const car_userID = e.target.id
-      if (auth.user?.id != car_userID) {
-        document.getElementById('allowed').innerText = "Permission refusée"
-        navigate('/login')
-      }
-      else {
-        // On réccupère l'élément sur lequel on clique
-        const ligne = e.currentTarget;
+      // On réccupère l'élément sur lequel on clique
+      const ligne = e.currentTarget;
 
-        const swalWithBootstrapButtons = Swal.mixin({
-          customClass: {
-            confirmButton: 'btn btn-success',
-            cancelButton: 'btn btn-danger'
-          }, 
-          buttonsStyling: false
+      const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        }, 
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Etes vous sûr ?',
+        text: "Action irréversible",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Oui',
+        cancelButtonText: 'Non',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+          axios.delete('http://127.0.0.1:8000/api/delete/'+e.target.id).then( (res) => {
+
+            if(res.data.status === 200){
+                
+              swalWithBootstrapButtons.fire(
+                'Supprimé!',
+                '',
+                'success'
+              )
+              ligne.closest('tr').remove();
+                
+            }
         })
-        
-        swalWithBootstrapButtons.fire({
-          title: 'Etes vous sûr ?',
-          text: "Action irréversible",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Oui',
-          cancelButtonText: 'Non',
-          reverseButtons: true
-        }).then((result) => {
-          if (result.isConfirmed) {
 
-            axios.delete('http://127.0.0.1:8000/api/delete/'+e.target.id).then( (res) => {
-
-              if(res.data.status === 200){
-                  
-                swalWithBootstrapButtons.fire(
-                  'Supprimé!',
-                  '',
-                  'success'
-                )
-                ligne.closest('tr').remove();
-                  
-              }
-          })
-
-          } else if (
-            result.dismiss === Swal.DismissReason.cancel
-          ) {
-            swalWithBootstrapButtons.fire(
-              'Annulé',
-              'Non supprimé',
-              'error'
-            )
-          }
-        })
-      }
+        } else if (
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Annulé',
+            'Non supprimé',
+            'error'
+          )
+        }
+      })
     }
 
     useEffect( () => {
     
-        axios.get('http://127.0.0.1:8000/api/cars').then( (res) => {
-            
-            setCars(res.data.cars);
-        
-        })
+      axios.get('http://127.0.0.1:8000/api/cars').then( (res) => {
+          
+          setCars(res.data.cars);
+      
+      })
         
     }, []);
   
@@ -93,8 +85,6 @@ function IndexCar() {
         {auth.user ? <h4 className="fw-normal mt-3">Bienvenue {auth?.user?.name}</h4> : ""}   
        
         <h1 className="mt-2">Liste des véhicules</h1>
-      
-        <span id='allowed' className='text-warning fw-bold'> </span>
       
         <table className="table mt-3">
             <thead>
@@ -117,7 +107,7 @@ function IndexCar() {
                   <td>{car.type}</td>
                   <td>{car.kilometrage}</td>
                   {auth.user?.id === car.user_id ? <td><Link type="button" to={auth.user?.id === car.user_id ? "/edit-car/"+car.id : "/login"} className="btn btn-success btn-sm">Modifier</Link></td> : <td></td>}
-                  {auth.user?.id === car.user_id ? <td><button type="button" id={car.user_id} onClick={deleteHandle} className="btn btn-danger btn-sm">Supprimer</button></td> : <td></td>}
+                  {auth.user?.id === car.user_id ? <td><button type="button" id={car.id} onClick={deleteHandle} className="btn btn-danger btn-sm">Supprimer</button></td> : <td></td>}
                 </tr>
               ))
             }
